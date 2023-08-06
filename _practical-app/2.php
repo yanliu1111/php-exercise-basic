1ab3554d3a2564d897e5c8dc4d227639e9a9a780
yanliu1111 <liuyan0504@gmail.com>
2023-08-06 15:26:22 -0600

    feat: second section done

--- _practical-app/2.php
+++ _practical-app/2.php
@@ -1,40 +1,37 @@
 <?php include "functions.php"; ?>
 <?php include "includes/header.php";?>
 
-	<section class="content">
+<section class="content">
 
-		<aside class="col-xs-4">
+    <aside class="col-xs-4">
 
-	<?php Navigation();?>
-			
-			
-		</aside><!--SIDEBAR-->
+        <?php Navigation();?>
 
 
-		<article class="main-content col-xs-8">
+    </aside>
+    <!--SIDEBAR-->
+
+
+    <article class="main-content col-xs-8">
+
+
+        <?php
+
+$number1 = 10;
+$number2 = 20;
+
+echo $number1 + $number2 . "<br>";
+
+$lists = array(21, 22, 23, 24, 25, 26, 27, 28, 29, 30);
+$lists_assoc = array("first_name" => 'Peter', "last_name" => 'Paker', "Age"=>18);
+echo $lists[0] . "<br>";
+echo $lists_assoc ["first_name"]. " " . $lists_assoc["last_name"]. " " .$lists_assoc["Age"]."<br>";
 		
-
-<?php
-
-
-
-		/* Step 1: Make 2 variables called number1 and number2 and set 1 to value 10 and the other 20:
-
-		  Step 2: Add the two variables and display the sum with echo:
-
-
-		  Step3: Make 2 Arrays with the same values, one regular and the other associative
-
-		 
-			 */
-
-		
-
-
 		?>
 
-	
 
-		</article><!--MAIN CONTENT-->
 
-<?php include "includes/footer.php"; ?>
+    </article>
+    <!--MAIN CONTENT-->
+
+    <?php include "includes/footer.php"; ?>
