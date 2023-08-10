4a716da7438bfad521230dfc4306f6ebfd61f9f3
yanliu1111 <liuyan0504@gmail.com>
2023-08-10 09:04:16 -0600

    feat: start mysql learn

---
+++ mysql/38_39_40_login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+
+</head>
+
+<body>
+    <div class="container">
+        <div class="col-xs-6">
+            <form action="">
+                <div class="form-group">
+                    <label for="username">Username</label>
+                    <input type="text" name="username" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="password">Password</label>
+                    <input type="password" name="password" class="form-control">
+                </div>
+                <input class="btn btn-primary" type="submit" name="submit" value="Submit">
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
