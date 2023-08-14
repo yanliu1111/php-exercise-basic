8b4b339d6de471ceccee65c56c3a3bc6ba7e7407
yanliu1111 <liuyan0504@gmail.com>
2023-08-14 07:12:06 -0600

    refactor: move functions

--- mysql/45_48login_update.php
+++ mysql/45_48login_update.php
@@ -1,17 +1,8 @@
 <?php include "45_db.php"; ?>
 <?php include "45_function.php";?>
-
 <?php
-if (isset($_POST['submit'])){
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-    $id = $_POST['id'];
-    $query = "UPDATE users SET username = '$username', password = '$password' WHERE id = $id";
-    $result = mysqli_query($connection, $query);
-    if (!$result) {
-        die("QUERY FAILED" . mysqli_error($connection));
-    }
-}
+if (isset($_POST['submit'])){}
+updateTable();
 ?>
 
 <!DOCTYPE html>
--- mysql/45_function.php
+++ mysql/45_function.php
@@ -5,7 +5,7 @@ global $connection;
 $query = "SELECT * FROM users";
 $result = mysqli_query($connection, $query);
 if (!$result) {
-    die('Query FAILED' . mysqli_error());
+    die('Query FAILED' . mysqli_error($connection));
 }
 
 
@@ -13,5 +13,18 @@ while($row = mysqli_fetch_assoc($result)) {
     $id = $row['id'];
     echo "<option value='$id'>$id</option>";
 }
+}
+
+function updateTable(){
+    global $connection;
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $id = $_POST['id'];
+    $query = "UPDATE users SET username = '$username', password = '$password' WHERE id = $id";
+    $result = mysqli_query($connection, $query);
+    if (!$result) {
+        die("QUERY FAILED" . mysqli_error($connection));
+    }
+
 }
 ?>
