1220b21e58987caefeeb4f53d89aff6faf4f23c3
yanliu1111 <liuyan0504@gmail.com>
2023-08-16 23:59:20 -0600

    feat: practice 7 done

--- _practical-app/7.php
+++ _practical-app/7.php
@@ -1,32 +1,40 @@
 <?php include "functions.php"; ?>
+<?php
+$connection = mysqli_connect('localhost', 'root', '', 'mylist');
+if(!$connection){
+	die('Database Connection Failed') . mysqli_error($connetion);
+} 
+
+$query = 'select * from reports';
+$result = mysqli_query($connection, $query);
+if (!$result){
+	die ("Query failed");
+}
+
+?>
 <?php include "includes/header.php";?>
-    
-
-	<section class="content">
-
-		<aside class="col-xs-4">
-
-		<?php Navigation();?>
-			
-			
-		</aside><!--SIDEBAR-->
 
 
-	<article class="main-content col-xs-8">
+<section class="content">
+
+    <aside class="col-xs-4">
+
+        <?php Navigation();?>
+
+
+    </aside>
+    <!--SIDEBAR-->
+
+
+    <article class="main-content col-xs-8">
+
+
+
+        <?php  
+while($record = mysqli_fetch_assoc($result)){
+ echo $record['days_of_the_week'];
+}
 	
-	
-	
-	<?php  
-
-	/*  Step 1 - Create a database in PHPmyadmin
-
-		Step 2 - Create a table like the one from the lecture
-
-		Step 3 - Insert some Data
-
-		Step 4 - Connect to Database and read data
-
-*/
 	
 	?>
 
@@ -34,6 +42,7 @@
 
 
 
-</article><!--MAIN CONTENT-->
+    </article>
+    <!--MAIN CONTENT-->
 
-<?php include "includes/footer.php"; ?>
+    <?php include "includes/footer.php"; ?>
