33e102f4672fe60656c98069545383668f9bf184
yanliu1111 <liuyan0504@gmail.com>
2023-08-22 23:27:08 -0600

    feat: function for login security

--- mysql/45_function.php
+++ mysql/45_function.php
@@ -6,10 +6,13 @@ if (isset($_POST['submit'])){
     global $connection;
     $username = $_POST['username'];
     $password = $_POST['password'];
+
+    $username = mysqli_real_escape_string($connection, $username );   
+    $password = mysqli_real_escape_string($connection, $password );
      
-     $query = "INSERT INTO users(username, password) ";
-     $query .= "VALUES ('$username', '$password')";
-     $result = mysqli_query($connection, $query);
+    $query = "INSERT INTO users(username, password) ";
+    $query .= "VALUES ('$username', '$password')";
+    $result = mysqli_query($connection, $query);
      if (!$result) {
          die('Query FAILED' . mysqli_error($connection));
      } else {
