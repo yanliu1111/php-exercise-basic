f90b18398d2a52f97c0ef0b49a6052241e57c37d
yanliu1111 <liuyan0504@gmail.com>
2023-08-23 23:11:21 -0600

    feat: done practice8

--- _practical-app/8.php
+++ _practical-app/8.php
@@ -1,21 +1,28 @@
 <?php include "functions.php"; ?>
 <?php include "includes/header.php";?>
 
-	<section class="content">
+<section class="content">
 
-		<aside class="col-xs-4">
+    <aside class="col-xs-4">
 
-		<?php Navigation();?>
-			
-			
-		</aside><!--SIDEBAR-->
+        <?php Navigation();?>
 
 
-		
-	<article class="main-content col-xs-8">
+    </aside>
+    <!--SIDEBAR-->
+
+
+
+    <article class="main-content col-xs-8">
+
+        <?php
+	$password = "secretpassword";
+	$password_encrypted = password_hash($password, PASSWORD_BCRYPT, array('cost' => 12));
+	echo $password_encrypted;
+
 	
-	
-	<?php  
+	?>
+        <?php  
 
 	/*  Step 1 -Make a variable with some text as value
 
@@ -33,5 +40,6 @@
 
 
 
-</article><!--MAIN CONTENT-->
-<?php include "includes/footer.php"; ?>
+    </article>
+    <!--MAIN CONTENT-->
+    <?php include "includes/footer.php"; ?>
