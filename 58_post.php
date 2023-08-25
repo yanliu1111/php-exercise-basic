cdb612354cb6a78ee6ee9130f3a8149c4b4634ac
yanliu1111 <liuyan0504@gmail.com>
2023-08-25 06:32:03 -0600

    feat:post

---
+++ 58_post.php
@@ -0,0 +1,26 @@
+<?php 
+
+$_POST['name'] = "SUPERMAN";
+
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <form action="58_post.php" method="post">
+        <input type="text" name="name">
+        <input type="submit">
+    </form>
+</body>
+
+</html>
