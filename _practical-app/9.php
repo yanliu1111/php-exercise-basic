a3f8ac88ec82089f43dd3118ce192d8c0bfc8e74
yanliu1111 <liuyan0504@gmail.com>
2023-08-26 23:24:38 -0600

    feat: session9 done

--- _practical-app/9.php
+++ _practical-app/9.php
@@ -1,23 +1,52 @@
 <?php include "functions.php"; ?>
 <?php include "includes/header.php";?>
 
+<?php
+
+session_start();
+
+$_SESSION['message'] = "You are logged in";
+
+$expiration =  time() + (60 * 60 * 24);
+setcookie("name", "value", $expiration);
+?>
+
+<section class="content">
+
+    <aside class="col-xs-4">
+
+        <?php Navigation();?>
 
 
-	<section class="content">
-
-		<aside class="col-xs-4">
-
-		<?php Navigation();?>
-			
-			
-		</aside><!--SIDEBAR-->
+    </aside>
+    <!--SIDEBAR-->
 
 
-			<article class="main-content col-xs-8">
-			
-		
-	
-	<?php 
+    <article class="main-content col-xs-8">
+
+        <?php
+		if (isset($_GET['source'])) {
+			echo $_GET['source'] . "<br>";
+		}
+		?>
+        <a href="9.php?source=30134">Click Here</a>
+        <br>
+        <?php
+        if (isset($_COOKIE['name'])) {
+        echo $_COOKIE['name'] . "<br>";
+        } else {
+        echo "No cookie" . "<br>";
+        }
+
+		if (isset($_SESSION['message'])) {
+			echo $_SESSION['message'];
+		}
+		?>
+
+
+
+
+        <?php 
 
 	/*  Create a link saying Click Here, and set 
 	the link href to pass some parameters and use the GET super global to see it
@@ -33,5 +62,6 @@
 
 
 
-</article><!--MAIN CONTENT-->
-<?php include "includes/footer.php"; ?>
+    </article>
+    <!--MAIN CONTENT-->
+    <?php include "includes/footer.php"; ?>
