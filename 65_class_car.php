b933ad481fda9be09a3b18de15038a8edd0ac8b9
yanliu1111 <liuyan0504@gmail.com>
2023-08-29 19:18:24 -0600

    feat:oop_class

---
+++ 65_class_car.php
@@ -0,0 +1,36 @@
+<?php
+class Car {
+    var $wheels = 4;
+    var $hood = 1;
+    var $engine = 1;
+    var $doors = 4;
+    function MoveWheels(){
+        $this -> wheels = 10;
+    }
+    function CreateDoors(){
+        $this -> doors = 6;
+    }
+}
+
+// if (class_exists("Car")) {
+//     echo "Class exists";
+// } else {
+//     echo "Class does not exist";
+// }
+// if (method_exists("Car", "MoveWheels")) {
+//     echo "Method exists";
+// } else {
+//     echo "Method does not exist";
+// }
+$bmw = new Car();
+$bmw -> MoveWheels();
+$truck = new Car();
+// $mercedes_benz = new Car();
+//use the functionality inside the class
+// $bmw->MoveWheels();
+// $mercedes_benz->MoveWheels();
+echo $bmw->wheels . "<br>";
+echo $truck->wheels = 10 . "<br>";
+$truck->CreateDoors();
+echo $truck->doors;
+?>
